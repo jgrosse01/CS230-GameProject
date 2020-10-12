68181afa45b7d3272cc55433a5a59668d71a76b0
Jake Grosse <j.grosse01@gmail.com>
2020-10-12 15:05:51 -0600

    Fixed merge errors

--- src/main/mainMenu.java
+++ src/main/mainMenu.java
@@ -1,6 +1,5 @@
 package main;
 
-<<<<<<< HEAD
 import javax.swing.*;
 import java.awt.*;
 
@@ -18,8 +17,5 @@ public class mainMenu extends JPanel{
 		gameContentPane.add(button, BorderLayout.CENTER);
 		gameContentPane.setVisible(true);
 	}
-=======
-public class mainMenu {
->>>>>>> 49f09d064a31248fa35ef8795707c44f8d66633b
-
 }
+
