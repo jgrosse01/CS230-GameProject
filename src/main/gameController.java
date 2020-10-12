655e036593a5153d5897d239615bde9f0443a374
birgirgauti <bstefansson@carroll.edu>
2020-10-12 14:57:33 -0600

    initial commit

---
+++ src/main/gameController.java
@@ -0,0 +1,47 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JButton;
+import javax.swing.JFrame; // for JFrame
+import javax.swing.JOptionPane; // messages are displayed using JOptionPane
+
+class gameController {
+	private JFrame gameJFrame;
+	boolean initialized;
+	String gameState;
+	mainMenu menu;
+	Container mainPane;
+	
+	JButton button;
+	
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    double width = screenSize.getWidth();
+    double height = screenSize.getHeight();
+
+	public gameController() {
+		
+		menu = new mainMenu();
+		gameJFrame = new JFrame();
+		gameJFrame.setTitle("GEHSUGHSEOISI");
+		gameJFrame.setSize((int) width, (int) height);
+        gameJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameJFrame.setVisible(true);
+        
+        mainPane = gameJFrame.getContentPane();
+        gameJFrame.setBackground(Color.red);
+        //menu.setBackground(Color.red);
+        mainPane.add(menu);
+        mainPane.setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+        gameController myController = new gameController();
+
+	}
+
+}
---
+++ src/main/mainMenu.java
@@ -0,0 +1,21 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class mainMenu extends JPanel{
+	JPanel gameContentPane;
+	ImageIcon image;
+	JButton button;
+	
+	public mainMenu() {
+		gameContentPane = new JPanel();
+		gameContentPane.setBorder(BorderFactory.createTitledBorder("TITTTLE"));
+        gameContentPane.setLayout(null);
+        this.setBackground(Color.green);
+		button = new JButton("FHIOEHSFOIJSOIFJE");
+		gameContentPane.add(button, BorderLayout.CENTER);
+		gameContentPane.setVisible(true);
+	}
+
+}
