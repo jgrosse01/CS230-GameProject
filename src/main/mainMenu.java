2ef06991e13d6c72db4c68c3fd819e6ebd6cdfe8
Jacob Grosse <jgrosse01@carroll.edu>
2020-10-13 11:45:11 -0600

    Fixed Warnings in gameController and mainMenu

--- src/main/mainMenu.java
+++ src/main/mainMenu.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class mainMenu extends JPanel{
+	private static final long serialVersionUID = 7962642755482788779L;
 	JPanel gameContentPane;
 	ImageIcon image;
 	JButton button;
