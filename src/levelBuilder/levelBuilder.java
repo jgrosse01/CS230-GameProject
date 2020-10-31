534a99f52bf89152e220c884873e296014e62d81
Matthew Bushnell <mbushnell@carroll.edu>
2020-10-31 14:22:28 -0600

    Added to levelBuilder, didn't change anything else.

--- src/levelBuilder/levelBuilder.java
+++ src/levelBuilder/levelBuilder.java
@@ -2,40 +2,127 @@ package levelBuilder;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class levelBuilder extends JPanel{
 	//JPanel wanted this
 	private static final long serialVersionUID = 2370201629065856700L;
 	
 	private JPanel tileSelect; //For holding all the swing objects that select the tile to place
-	private JPanel level; //For holding all the slots for actually placing a tile in the level
-	private JComboBox tileCB;
-	private JComboBox entityCB;
-	private JComboBox puzzleCB;
+	private JPanel levelPanel;
+	private JComboBox tileCB; //For selecting specific tile in your tile group
+	private ButtonGroup radioButtons; //For selecting a tile group i.e. tiles, entities, puzzles
+	private String[][] levelArray;
+	private JFrame thisFrame;
 	
-	public levelBuilder() {
+	public levelBuilder(JFrame frame) {
 		//Creating the class variables
 		tileSelect = new JPanel();
-		level = new JPanel();
+		levelPanel = new JPanel();
+		
+		thisFrame = frame;
 		
 		this.setBackground(Color.red); //CHANGE THIS EVENTUALLY
 		this.setLayout(new BorderLayout());
 		this.setVisible(true);
 		
+		//Radio buttons for selecting tile groups
+		JRadioButton radioTile = new JRadioButton("Tiles");
+		JRadioButton radioPuzzle = new JRadioButton("Puzzles");
+		JRadioButton radioEntity = new JRadioButton("Entities");
+		radioButtons = new ButtonGroup();
+		radioButtons.add(radioTile);
+		radioButtons.add(radioPuzzle);
+		radioButtons.add(radioEntity);
+		
+		//Buttons for selecting options
+		JButton newButton = new JButton("New Level");
+		newButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+					makeNewLevel();
+			}		
+		});
+		JButton saveButton = new JButton("Save Level");
+		saveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				makeButton();
+			}
+		});
+		
+		
+		//combobox for selecting the tiles
+		String[] tiles = {"1", "2", "3", "4"}; //using strings for testing
+		tileCB = new JComboBox(tiles);
+		tileCB.setSelectedIndex(0);
+		
 		//formatting tileSelect and then adding it to the Panel
-		tileSelect.setLayout(new FlowLayout());
+		//tileSelect.setLayout(new FlowLayout());
 		tileSelect.setBorder(BorderFactory.createTitledBorder("Tile Select"));
 		tileSelect.setVisible(true);
-		this.add(tileSelect, BorderLayout.WEST);
+		tileSelect.setBounds(0,0,thisFrame.getWidth(), (int)(thisFrame.getHeight()*.15));
+		this.add(tileSelect, BorderLayout.NORTH);
+		//Adding combobox and buttons to the tileSelect
+		tileSelect.add(newButton);
+		tileSelect.add(saveButton);
+		tileSelect.add(tileCB);
+		tileSelect.add(radioTile);
+		tileSelect.add(radioPuzzle);
+		tileSelect.add(radioEntity);
+		radioTile.setSelected(true);
+		tileCB.setVisible(true);
 		
 		//^^ but for level object
-		level.setLayout(new FlowLayout());
-		this.add(level, BorderLayout.EAST);
+		//level.setLayout(new FlowLayout());
+		this.add(levelPanel, BorderLayout.CENTER);
+		levelPanel.setBackground(Color.black);
 		
 		//Creating combo boxes for tileSelect. Needs to be integrated with
 		//all the different tiles when we have them made.
 		tileCB = new JComboBox();
+		
 	}
+	
+	//this will populate the levelArray for use in making a level.
+	//It is called when the "new level" button is pushed.
+	private void makeNewLevel()
+	{
+		//Creating a panel with two text fields for the popup message
+		JTextField widthField = new JTextField(3);
+		JTextField heightField = new JTextField(3);
+		int width;
+		int height;
+		
+		JPanel panel = new JPanel();
+		panel.add(new JLabel("Width: "));
+		panel.add(widthField);
+		panel.add(new JLabel("Height: "));
+		panel.add(heightField);
+		
+		int response = JOptionPane.showConfirmDialog(null, panel, "Enter width and height for the level", 
+				JOptionPane.OK_CANCEL_OPTION);
+		width = Integer.parseInt(widthField.getText());
+		height = Integer.parseInt(heightField.getText());
+		if(response == JOptionPane.OK_OPTION)
+		{
+			levelArray = new String[width][height];
+			paintLevel(levelPanel.getGraphics());
+		}
+	}
+	
+	//Dumb method for testing delete later
+	private void makeButton()
+	{
+		levelPanel.add(new JButton("dumb"));
+	}
+	
+	private void paintLevel(Graphics g)
+	{
+		int width = levelArray.length;
+		int height = levelArray[0].length;
+		g.setColor(Color.blue);
+		g.fillRect(200, 200, width, height);
+}
 	
 	/**
 	 * This stuff is just for testing, can be removed once the main menu
@@ -47,9 +134,12 @@ public class levelBuilder extends JPanel{
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.setSize(600,600);
-        levelBuilder build = new levelBuilder();
+        frame.setTitle("Testing Frame");
+        levelBuilder build = new levelBuilder(frame);
         frame.add(build);
         build.setSize(frame.getSize());
+        frame.revalidate();
+        frame.repaint();
 	}
 	
 }
