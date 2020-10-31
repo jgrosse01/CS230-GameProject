e09caf98eeef789bfa8ff4706bb9f6165e34b635
birgirgauti <bstefansson@carroll.edu>
2020-10-31 14:33:12 -0600

    Updates to Player and Entity

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -1,10 +1,14 @@
 package entities;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class Player extends Entity implements KeyListener {
@@ -12,6 +16,7 @@ public class Player extends Entity implements KeyListener {
 	private int dx; //Direction x (change)
     private int dy; //Direction y (change)
     private boolean isAirbourne = false; //to utilize timer to set limit on jump
+    private BufferedImage[] playerImages;
     //put timer and keep track in the controlling display class to set dy to -2 for time equivalent to time moving up (Super Mario Feel)
 	
     public Player(int x, int y, BufferedImage imageLeft, BufferedImage imageRight, JPanel pane) throws IOException{
@@ -25,6 +30,22 @@ public class Player extends Entity implements KeyListener {
     public void setDY(int dY) { this.dy = dY; }
     
     public boolean isAirbourne() { return isAirbourne; }
+    
+    public void loadImage(String fileName)
+	{
+		if (fileName != null)
+			try
+			{
+				for (int i = 0; i<83; i++) {
+					playerImages[i] = ImageIO.read(new File("sprites/" + fileName));
+				}
+			}
+			catch (IOException e)
+			{
+				System.out.println("Image not found.");
+			}
+		
+	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
