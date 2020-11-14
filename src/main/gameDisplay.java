633e1bf5c12f45cf55ba9c22d5ed76df9776e1b7
Jacob Grosse <jgrosse01@carroll.edu>
2020-11-14 12:50:16 -0700

    create constructor in gameDisplay

--- src/main/gameDisplay.java
+++ src/main/gameDisplay.java
@@ -2,6 +2,19 @@ package main;
 
 import javax.swing.JPanel;
 
+import levelBuilder.levelInfo;
+
 public class gameDisplay extends JPanel{
 
+	private static final long serialVersionUID = -3506813690426695567L;
+	
+	levelInfo currentLevel;
+
+	public gameDisplay(gameController frame) {
+		
+	}
+	
+	private void paint() {
+		
+	}
 }
