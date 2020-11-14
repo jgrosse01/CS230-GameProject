67b47f634d11a8843d9b15915dccad0a43773768
Jacob Grosse <jgrosse01@carroll.edu>
2020-11-14 15:32:35 -0700

    levelloader?

--- src/levelBuilder/LevelLoader.java
+++ src/levelBuilder/LevelLoader.java
@@ -7,12 +7,13 @@ import main.gameController;
 
 import javax.swing.JPanel;
 
-import tiles.Dirt;
-import tiles.Tile;
+import tiles.*;
 
 public class LevelLoader {
 
 	public static levelInfo load(String levelString, JPanel pane) { 
+		
+		//This splits first line by colon for width and height
 		Tile[][] levelArray;
 		levelInfo level;
 		int width;
@@ -25,6 +26,9 @@ public class LevelLoader {
 		String[] lineSplit = line.split(":");
 		width = Integer.parseInt(lineSplit[0]);
 		height = Integer.parseInt(lineSplit[1]);
+		
+		//Split by spaces to get tiles
+		
 		} catch (IOException e){
 			e.printStackTrace();
 		}
@@ -33,20 +37,24 @@ public class LevelLoader {
 	
 	//stringToTile for "normal" tiles
 	private static Tile stringToTile(String string, int x, int y, JPanel pane) {
-		if(string == "Dirt") {return new Dirt(x,y,pane);};
+		switch (string) {
+		case "Dirt": return new Dirt(x,y,pane);
+		case "Metal": return new Metal(x,y,pane);
+		}
 
 		return null;
 	}
 	
 	//stringToTile for tiles that have extra options that we need to know
 	private static Tile stringToTile(String string, int x, int y, JPanel pane, String options) {
-		if(string == "SpawnPoint") {
-			
+		if (options.contains(":")) {
+			String[] splitOptions = options.split(":");
 		}
+		
 		return null;
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("yo");
+		System.out.println("Comment Ya Code");
 	}
 } 
